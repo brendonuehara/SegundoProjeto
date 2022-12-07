96272d650072a8c9f5a7b53890556a9d409e5636
Brendon Uehara <brendon.uehara@gmail.com>
2022-12-07 17:59:10 -0300

    first commit

---
+++ assets/scripts/script.js
@@ -0,0 +1,35 @@
+// Opacity icon whatsapp
+let iconClose = document.getElementById("icon-whatsapp-close");
+let spanWhatsapp = document.getElementById("span-whatsapp");
+let emoteWhatsapp = document.getElementById("emote-whatsapp");
+
+spanWhatsapp.addEventListener("mouseover", () => {
+  iconClose.style.opacity = 0.4;
+});
+
+iconClose.addEventListener("mouseover", () => {
+  iconClose.style.opacity = 1;
+});
+
+iconClose.addEventListener("mouseleave", () => {
+  iconClose.style.opacity = 0;
+});
+
+spanWhatsapp.addEventListener("mouseleave", () => {
+  iconClose.style.opacity = 0;
+});
+//
+
+// Closed span whatsapp
+
+function closeSpan() {
+  setTimeout(() => {
+    spanWhatsapp.style.display = "none";
+    iconClose.style.display = "none";
+    emoteWhatsapp.style.display = "none";
+  }, 300)
+}
+
+iconClose.addEventListener("click", closeSpan);
+
+//
